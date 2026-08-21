7b146afb57b3d7e1afa2cbe4af03577c052567df
Armaan Amoli <armaanmohanamoli@gmail.com>
2026-08-21 00:58:40 +0000

    basic api connection

--- packages/agent/loop.ts
+++ packages/agent/loop.ts
@@ -1,15 +1,37 @@
 import OpenAI from 'openai';
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+import type { ChatCompletionMessageParam } from 'openai/resources';
 
 const client = new OpenAI({
   apiKey: process.env['NVIDIA_API_KEY'],
+  baseURL: 'https://nvidia.com',
 });
 const instructions = `
 You are Baby Panda a coding agent
 `
-const model = 'z-ai/glm-5.2';
+const model = 'mistralai/mistral-nemotrons';
 
+while (true) {
+  const rl = readline.createInterface({ input, output });
 
-const response = await client.responses.create({
-  model: model,
-  instructions: instructions,
-});
+  const messages: ChatCompletionMessageParam[] = // this array will store message history for context building
+  [
+    { role: 'system', content: instructions },
+  ]
+
+  const userInput = await rl.question('');
+
+  messages.push({ role: 'user', content: userInput })
+
+  const response = await client.chat.completions.create({
+    model: model,
+    messages: messages,
+    store:false
+  });
+
+  if (typeof response._request_id !== null) {
+    console.log(response.choices[0]);
+  }
+
+}
