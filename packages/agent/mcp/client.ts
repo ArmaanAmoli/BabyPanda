334f3e8a7c732313cf3b982a32d1392aa53a159f
Armaan Amoli <armaanmohanamoli@gmail.com>
2026-09-12 00:22:06 +0000

    fix(MCP): delete tool schema

--- packages/agent/mcp/client.ts
+++ packages/agent/mcp/client.ts
@@ -15,6 +15,7 @@ export class MCPClient {
             if (!isJs) {
                 throw new Error("Server script must be a .ts file");
             }
+            console.log("MCP:", cwd)
             this.transport = new StdioClientTransport({
                 command: "npx",
                 args: ["tsx", serverScriptPath],
@@ -24,18 +25,22 @@ export class MCPClient {
                     CLIENT_CWD: cwd
                 }
             });
+            console.log("StdioClientTreansportCreated")
             await this.mcp.connect(this.transport);
+            console.log("connected")
             const toolsResult = await this.mcp.listTools();
+            console.log(toolsResult)
             this.tools = toolsResult.tools.map((tool) => {
+                console.log(tool)
                 return {
                     name: tool.name,
                     description: tool.description,
                     input_schema: tool.inputSchema
                 };
             });
-            console.log("Connected to server with tools: ", this.tools.map(({ name }: any) => { name }))
+            console.log("Connected to server with tools: ", this.tools.map( ({ name }: any) => { name } ) )
         } catch (err) {
-            console.log("Failed to connect to MCP server: ", err);
+            console.log("[ERROR] packages/agent/mcp/client.ts Failed to connect to MCP server: ", err);
             throw err;
         }
     }
--- packages/agent/mcp/index.ts
+++ packages/agent/mcp/index.ts
@@ -3,11 +3,10 @@ import {StdioServerTransport} from "@modelcontextprotocol/server/stdio";
 import {read , grep , edit , glob , del} from './tools/filesystem'
 import {array, string, z} from "zod";
 
-const cwd = process.env.CLIENT_CWD || process.cwd();
 const server = new McpServer({
     name:"baby-panda/mcp",
     version:"1.0.0",
-})
+});
 
 server.registerTool(
     "read",
@@ -69,7 +68,7 @@ server.registerTool(
             ]
         }
     }
-)
+);
 
 server.registerTool(
     "glob",
@@ -88,7 +87,7 @@ server.registerTool(
             ]
         }
     }
-)
+);
 
 server.registerTool(
     "delete",
@@ -97,10 +96,10 @@ server.registerTool(
         inputSchema:z.object({
             path:string().describe("path of the file"),
             options:z.object({
-                force:z.boolean().or(z.undefined()).optional().default(false),
-                maxRetries:z.number().or(z.undefined()).optional().default(0),
-                recursive:z.boolean().or(z.undefined()).optional().default(false),
-                retryDelay:z.number().or(z.undefined()).default(100),
+                force:z.boolean().optional().default(false),
+                maxRetries:z.number().optional().default(0),
+                recursive:z.boolean().optional().default(false),
+                retryDelay:z.number().default(100),
             }).optional()
         }),
     },
@@ -112,7 +111,7 @@ server.registerTool(
             ]
         }
     }
-)
+);
 
 async function main(){
     const transport = new StdioServerTransport();
