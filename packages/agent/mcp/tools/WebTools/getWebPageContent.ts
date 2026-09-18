29f0c90b45ceff2af374e76bf0bf273a4be38673
Armaan Amoli <armaanmohanamoli@gmail.com>
2026-09-18 09:15:08 +0000

    add: validation for getWebPage() function

--- packages/agent/mcp/tools/WebTools/getWebPageContent.ts
+++ packages/agent/mcp/tools/WebTools/getWebPageContent.ts
@@ -2,15 +2,30 @@ import Exa from "exa-js";
 import * as dotenv from 'dotenv';
 import * as path from 'path';
 import { fileURLToPath } from 'url';
+import { z } from "zod";
 //to be removed
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({path:path.resolve(__dirname , '../../../.env')})
+dotenv.config({ path: path.resolve(__dirname, '../../../.env') })
 //
 
 const exa = new Exa();
+const WebPageResultsSchema = z.array(z.object({
+    id: z.string(),
+    title: z.string().nullable().catch(null),
+    url: z.string(),
+    author: z.string().nullable().catch(null),
+    text: z.string(),
+    extras: z.object({
+        links: z.array(z.string()).default([])
+    }),
+    entities: z.object().nullable().catch(null)
 
-export async function getWebPage(url:string[]) {
+}));
+
+type WebPageResults = z.infer<typeof WebPageResultsSchema>;
+
+export async function getWebPage(url: string[]): Promise<WebPageResults> {
     const result = await exa.getContents(
         url, {
         text: true,
@@ -19,5 +34,11 @@ export async function getWebPage(url:string[]) {
         },
     }
     );
-    return result.results;
+    const results = result.results;
+    try {
+        const parsed = WebPageResultsSchema.parse(results)
+        return parsed;
+    } catch (err) {
+        throw new Error(`Error occured while parsing results from WebPage: ${err}`)
+    }
 }
